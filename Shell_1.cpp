f40ee275bb184b7e8f5fec60c94ccfd34086cc35
DimaPo <135866428+DimaPO404@users.noreply.github.com>
2025-10-17 10:10:07 +0300

    Shell (task 2)
    Изменил код на вывод введённой строки в цикле и выход по нажатию Ctrl+D

--- Shell_1.cpp
+++ Shell_1.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int main() {
     string input;
+    
     cout << "Введите строку: ";
-    getline(cin, input);
-    cout << input << endl;
+    while (getline(cin, input)) {
+        cout << input << '\n';
+        cout << "Введите строку: ";
+    }
+    
+    cout << "\nВыход по нажатию Ctrl+D" << '\n';
     return 0;
 }
