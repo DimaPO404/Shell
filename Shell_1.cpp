4e91281295747b51fecb113a0f0ded2562da2705
DimaPo <135866428+DimaPO404@users.noreply.github.com>
2025-10-17 10:00:59 +0300

    Add files via upload

---
+++ Shell_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string input;
+    cout << "Введите строку: ";
+    getline(cin, input);
+    cout << input << endl;
+    return 0;
+}
